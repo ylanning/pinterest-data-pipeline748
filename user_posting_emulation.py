c7ee4c6cded7492e4a57faddc2155e4e52021700
ysuryati <yanti.suryati@framestore.com>
2023-12-28 14:24:12 +0000

    Configured AWS API Gateway and Mounted S3 buckets to Databricks

--- user_posting_emulation.py
+++ user_posting_emulation.py
@@ -7,7 +7,8 @@ import json
 import sqlalchemy
 from sqlalchemy import text
 from pprint import pprint
-
+from datetime import datetime
+import ast
 
 random.seed(100)
 
@@ -29,95 +30,134 @@ class AWSDBConnector:
 
 new_connector = AWSDBConnector()
 
-invoke_url =  "https://uf6d6cgu5e.execute-api.us-east-1.amazonaws.com/topics"
-# pin_invoke_url = f"{invoke_url}/0a2f66c3e41f.pin"
-# geo_invoke_url = f"{invoke_url}/0a2f66c3e41f.geo"
-# user_invoke_url = f"{invoke_url}/0a2f66c3e41f.user"
+invoke_url =  "https://uf6d6cgu5e.execute-api.us-east-1.amazonaws.com/pinterest/topics"
+pin_invoke_url = f"{invoke_url}/0a2f66c3e41f.pin"
+geo_invoke_url = f"{invoke_url}/0a2f66c3e41f.geo"
+user_invoke_url = f"{invoke_url}/0a2f66c3e41f.user"
 
 # Function to send data to Kafka topic via API Gateway
-def send_data_to_kafka_topic(url,data):
-    headers = {"Content-Type": "application/vnd.kafka.json.v2+json"}
-    response = requests.request("GET", invoke_url, params=data, headers=headers) 
+
+def datetime_serializer(obj):
+    if isinstance(obj,datetime):
+        return obj.isoformat()
+    raise TypeError("Type not serializeable")
+    
+def send_data_to_kafka_topic(url,datas):
+    print("++++++"  * 25)
+    print('send data to kafka topics')
+    
+    headers = {'Content-Type': 'application/vnd.kafka.json.v2+json'}    
+    response = requests.request("POST", url, data=datas, headers=headers)
     
     if response.status_code == 200:
         print(f"Data sent successfully to {url}")
     else:
         print(f"Failed to send data to {url}. Status code: {response.status_code}, Response: {response.text}")
 
-    print(response.status_code)
-
 def run_infinite_post_data_loop():
     print('inside run infinite post data loop')
-    while True:
-        sleep(random.randrange(0, 2))
-        random_row = random.randint(0, 11000)
-        engine = new_connector.create_db_connector()
+    counter = 0
+    while counter < 25:
+        try:
+            sleep(random.randrange(0, 2))
+            random_row = random.randint(0, 11000)
+            engine = new_connector.create_db_connector()
 
-        with engine.connect() as connection:
+            with engine.connect() as connection:
+                ######### PIN ##########
+                pin_string = text(f"SELECT * FROM pinterest_data LIMIT {random_row}, 1")
+                pin_selected_row = connection.execute(pin_string)
+                
+                for row in pin_selected_row:
+                    pin_result = dict(row._mapping)
+                    print(pin_result)
 
-            pin_string = text(f"SELECT * FROM pinterest_data LIMIT {random_row}, 1")
-            pin_selected_row = connection.execute(pin_string)
-            
-            for row in pin_selected_row:
-                pin_result = dict(row._mapping)
+                payload = json.dumps({
+                "records": [
+                    {
+                    #Data should be send as pairs of column_name:value, with different columns separated by commas
+                    "value": {
+                            "index": pin_result["index"], 
+                            "category": pin_result["category"], 
+                            "downloaded": pin_result["downloaded"], 
+                            "follower_count": pin_result["follower_count"],
+                            "image_src": pin_result["image_src"],
+                            "is_image_or_video": pin_result["is_image_or_video"],
+                            "poster_name": pin_result["poster_name"],
+                            "save_location": pin_result["save_location"],
+                            "tag_list": pin_result["tag_list"],
+                            "title": pin_result["title"],
+                            "unique_id": pin_result["unique_id"]}
+                    }]})
 
-            # Send data to Kafka topics via API Gateway
-            send_data_to_kafka_topic(invoke_url,pin_result)
+                print('PAYLOAD')
+                print(payload)
 
-            # geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
-            # geo_selected_row = connection.execute(geo_string)
-            
-            # for row in geo_selected_row:
-            #     geo_result = dict(row._mapping)
+                # Send data to Kafka topics via API Gateway
+                send_data_to_kafka_topic(pin_invoke_url,payload)
 
-            # user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
-            # user_selected_row = connection.execute(user_string)
-            
-            # for row in user_selected_row:
-            #     user_result = dict(row._mapping)
-            
-            # pprint(pin_result)
-            # print("\n")
-            # print("\n")
+                ######### GEO ##########
+                geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
+                geo_selected_row = connection.execute(geo_string)
+                
+                for row in geo_selected_row:
+                    geo_result = dict(row._mapping)
 
-            # {'category': 'mens-fashion',
-            # 'description': 'No description available Story format',
-            # 'downloaded': 0,
-            # 'follower_count': 'User Info Error',
-            # 'image_src': 'Image src error.',
-            # 'index': 7528,
-            # 'is_image_or_video': 'multi-video(story page format)',
-            # 'poster_name': 'User Info Error',
-            # 'save_location': 'Local save in /data/mens-fashion',
-            # 'tag_list': 'N,o, ,T,a,g,s, ,A,v,a,i,l,a,b,l,e',
-            # 'title': 'No Title Data Available',
-            # 'unique_id': 'fbe53c66-3442-4773-b19e-d3ec6f54dddf'}
+                
+                # print(type(geo_result))
+                # print(geo_result)
 
-            # pprint(geo_result)
-            # print("\n")
-            # print("\n")
+                payload = json.dumps({
+                "records": [
+                    {
+                    #Data should be send as pairs of column_name:value, with different columns separated by commas
+                    "value": {"index": geo_result["ind"], 
+                            "timestamp": geo_result["timestamp"].isoformat(), 
+                            "latitude": geo_result["latitude"], 
+                            "longitude": geo_result["longitude"],
+                            "country": geo_result["country"]}
+                    }]})
 
-            # {'country': "Cote d'Ivoire",
-            # 'ind': 2923,
-            # 'latitude': -84.6302,
-            # 'longitude': -164.507,
-            # 'timestamp': datetime.datetime(2019, 9, 8, 22, 53, 9)}
+                print('PAYLOAD')
+                print(payload)
+                    
+                # Send data to Kafka topics via API Gateway
+                send_data_to_kafka_topic(geo_invoke_url,payload)
 
-            # pprint(user_result)
-            # print("\n")
-            # print("\n")
-            
-            # {'age': 36,
-            # 'date_joined': datetime.datetime(2015, 12, 8, 20, 2, 43),
-            # 'first_name': 'Rachel',
-            # 'ind': 5730,
-            # 'last_name': 'Davis'}
+                ######### USER ##########
+                user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
+                user_selected_row = connection.execute(user_string)
+                
+                for row in user_selected_row:
+                    user_result = dict(row._mapping)
+                    print(type(user_result))
+                    print(user_result)
+                                    
+                payload = json.dumps({
+                "records": [
+                    {
+                    #Data should be send as pairs of column_name:value, with different columns separated by commas
+                    "value": {"index": user_result["ind"], 
+                            "first_name": user_result["first_name"], 
+                            "last_name": user_result["last_name"], 
+                            "age": user_result["age"],
+                            "date_joined": user_result["date_joined"].isoformat()}
+                    }]})
 
+                print('PAYLOAD')
+                print(payload)
+                   
+                # Send data to Kafka topics via API Gateway
+                send_data_to_kafka_topic(user_invoke_url,payload)
+                
+            counter += 1
+                
+        except KeyboardInterrupt:
+             # Handle a keyboard interrupt (the user presses Ctrl+C)
+                print("\nExiting the loop.")
+                break  # Exit the loop
+                
 
 if __name__ == "__main__":
     run_infinite_post_data_loop()
     print('Working')
-    
-    
-
-
